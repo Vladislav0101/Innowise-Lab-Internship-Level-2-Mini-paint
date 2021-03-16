51513b0ec91e6225aabd51364533ca5ce10be785
Vladislav0101 <by.vlad01@mail.ru>
2021-03-16 12:34:25 +0300

    vue.config

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  productionSourceMap: false,
+  // publicPath:
+  //   process.env.NODE_ENV === "production"
+  //     ? "/Innowise-Lab-Internship-Level-2-Mini-paint/"
+  //     : "/"
+};
